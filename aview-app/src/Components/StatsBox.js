91cb587a7b33e3608698604afeaa4dc49cbd3a2d
apar-mistry <amist007@ucr.edu>
2022-03-16 17:41:44 -0700

    Added StatBar missing animations - add later

--- aview-app/src/Components/StatsBox.js
+++ aview-app/src/Components/StatsBox.js
@@ -5,7 +5,13 @@ function StatsBox(){
     return(
         <statsBox class = 'statBox'>
             <header className="rect1"></header>
-            <header className="rect2"></header>    
+            <header className="rect2"></header> 
+            <header className="stat1">250M+</header>   
+            <p className="stat1under">Lorem ipsum</p>
+            <header className="stat2">15M+</header>   
+            <p className="stat2under">Lorem ipsum</p>
+            <header className="stat3">10M+</header>   
+            <p className="stat3under">Lorem ipsum</p>
         </statsBox>
 
 
