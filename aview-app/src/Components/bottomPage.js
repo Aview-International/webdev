09e48e38b53cb01e3d21686dfc3ecc2feef82ae9
apar-mistry <amist007@ucr.edu>
2022-03-18 18:22:26 -0700

    Added logo

---
+++ aview-app/src/Components/bottomPage.js
@@ -0,0 +1,14 @@
+import "../Styles/bottomPage.css";
+import React from "react";
+import Button from '@mui/material/Button';
+
+function BottomPage(){
+    return(
+        <bottomPage class = 'pageBot'>
+            <header className="aviewLogo"></header>
+        </bottomPage>
+    );
+
+
+}
+export default BottomPage;
