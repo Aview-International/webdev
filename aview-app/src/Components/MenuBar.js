fde7022e376ad93460e79cf48a8cf36b273dc38d
mikehudz <mikehudz7@hotmail.com>
2022-03-31 15:10:39 -0600

    centering changes

--- aview-app/src/Components/MenuBar.js
+++ aview-app/src/Components/MenuBar.js
@@ -3,7 +3,7 @@ import {useNavigate} from "react-router-dom";
 import Button from '@mui/material/Button';
 import React from "react";
 import box from "../img/Rectangle-67.svg";
-
+import Logo from '../img/logowhite3.svg';
 
 
 function MenuBar(){
@@ -21,86 +21,107 @@ function MenuBar(){
 
 
     return(
-        <menuBar class = 'bar'>
-            <header className='homeBut'>
-                <Button href="#text-buttons" style={{
- 
-                    // fontFamily: 'Overpass',
-                    // fontStyle: 'normal',
-                    fontWeight: 500,
-                    fontSize: '18px',
-                    lineHeight: '30px',
-                    textTransform: 'none',
-                    color: '#FFFFFF'
 
-                 }}onClick={homeRoute} disableRipple>
-                    Home
-                </Button>
-            </header>
-            <header className='creatorBut'>
-                <Button href="#text-buttons" style={{
- 
-                    // fontFamily: 'Overpass',
-                    // fontStyle: 'normal',
-                    fontWeight: 500,
-                    fontSize: '18px',
-                    lineHeight: '30px',
-                    textTransform: 'none',
-                    color: '#FFFFFF'
+        <div className="menuBarContainer">
+            
 
-                 }}disableRipple>
-                    Creators
-                </Button>
-            </header>
-            <header className='translatorBut'>
-                <Button href="#text-buttons" style={{
- 
-                    // fontFamily: 'Overpass',
-                    // fontStyle: 'normal',
-                    fontWeight: 500,
-                    fontSize: '18px',
-                    lineHeight: '30px',
-                    textTransform: 'none',
-                    color: '#FFFFFF'
+            
+                <img src={Logo} className="Landing-logo" alt="logo" ></img>
+                <p>Home</p>
+                <p>Creators</p>
+                <p>Translators</p>
+                <p>About</p>
+                <button>Contact Us</button>
 
-                 }}>
-                    Translators
-                </Button>
-            </header> 
-            <header className='aboutBut'>
-                <Button variant = 'text' style={{
- 
-                    // fontFamily: 'Overpass',
-                    // fontStyle: 'normal',
-                    
-                    fontWeight: 500,
-                    fontSize: '18px',
-                    lineHeight: '22px',
-                    textTransform: 'none',
-                    color: '#FFFFFF'
 
-                 }} onClick={aboutRoute}>
-                    About
-                </Button>
-            </header>
-            {/* <header className="contactRect"></header> */}
-            <header className='contactRect'> <img src={box} className="box1" alt="contactRect" ></img></header>
 
-            <header className='contactBut'>
-                <Button variant = "text" href="#text-buttons" sx={{
-                    width: '200px',
-                    fontWeight: 500,
-                    fontSize: '18px',
-                    lineHeight: '30px',
-                    color: '#ffffff',
-                    textTransform: 'none'  
+                {/* <header className='homeBut'>
+                    <Button href="#text-buttons" style={{
+    
+                        // fontFamily: 'Overpass',
+                        // fontStyle: 'normal',
+                        fontWeight: 500,
+                        fontSize: '18px',
+                        lineHeight: '30px',
+                        textTransform: 'none',
+                        color: '#FFFFFF'
 
-                 }}onClick={()=>{window.location.reload(false);}}disableRipple disableFocusRipple>
-                    Contact Us
-                </Button>
-            </header> 
+                    }}onClick={homeRoute} disableRipple>
+                        Home
+                    </Button>
+                </header>
 
-        </menuBar>
+                <header className='creatorBut'>
+                    <Button href="#text-buttons" style={{
+    
+                        // fontFamily: 'Overpass',
+                        // fontStyle: 'normal',
+                        fontWeight: 500,
+                        fontSize: '18px',
+                        lineHeight: '30px',
+                        textTransform: 'none',
+                        color: '#FFFFFF'
+
+                    }}disableRipple>
+                        Creators
+                    </Button>
+                </header>
+
+                <header className='translatorBut'>
+                    <Button href="#text-buttons" style={{
+    
+                        // fontFamily: 'Overpass',
+                        // fontStyle: 'normal',
+                        fontWeight: 500,
+                        fontSize: '18px',
+                        lineHeight: '30px',
+                        textTransform: 'none',
+                        color: '#FFFFFF'
+
+                    }}>
+                        Translators
+                    </Button>
+                </header> 
+
+                <header className='aboutBut'>
+                    <Button variant = 'text' style={{
+    
+                        // fontFamily: 'Overpass',
+                        // fontStyle: 'normal',
+                        
+                        fontWeight: 500,
+                        fontSize: '18px',
+                        lineHeight: '22px',
+                        textTransform: 'none',
+                        color: '#FFFFFF'
+
+                    }} onClick={aboutRoute}>
+                        About
+                    </Button>
+                </header> */}
+
+            
+
+            
+                    {/* <header className="contactRect"></header> */}
+                {/* <header className='contactRect'> <img src={box} className="box1" alt="contactRect" ></img></header>
+
+                <header className='contactBut'>
+                    <Button variant = "text" href="#text-buttons" sx={{
+                        width: '200px',
+                        fontWeight: 500,
+                        fontSize: '18px',
+                        lineHeight: '30px',
+                        color: '#ffffff',
+                        textTransform: 'none'  
+
+                     }}onClick={()=>{window.location.reload(false);}}disableRipple disableFocusRipple>
+                        Contact Us
+                    </Button>
+                </header>  */}
+            
+
+        </div>
 
 
     );
--- aview-app/src/Pages/Landing.js
+++ aview-app/src/Pages/Landing.js
@@ -3,7 +3,6 @@ import React, {useState, useEffect} from "react";
 import Background from "../Components/Background.js";
 import MenuBar from '../Components/MenuBar.js';
 import StatsBox from '../Components/StatsBox.js';
-import Logo from '../img/logowhite3.svg';
 import TopText from '../Components/TopText';
 import Graphics from '../Components/Graphics';
 import GlobeInfo from '../Components/globeInfo';
@@ -15,10 +14,9 @@ const baseURL = "http://localhost:3000";
 
 function Landing(){
     return(
-        <div>
+        <div className='landing'>
 
             <Background></Background>
-            <header className='logo'> <img src={Logo} className="Landing-logo" alt="logo" ></img></header>
             <MenuBar/>
             <Graphics/>
             <StatsBox/>
